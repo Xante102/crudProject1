368ea0a95bcd8f6950fc9c012c57cfaa62ea651d
Xante Graham <xantegraham.14@gmail.com>
2022-05-10 00:32:07 -0500

    projects updated

--- routes/notes.js
+++ routes/notes.js
@@ -1,55 +1,65 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var connection  = require('../lib/db');
- 
- 
+var connection = require("../lib/db");
+
 /* GET notes page. */
-router.get('/', function(req, res, next) {
-      
- connection.query('SELECT * FROM notes ORDER BY id',function(err, rows)     {
- 
-        if(err){
-            //req.flash('error', err); 
-            res.render('notes',
-            {
-                page_title: "Notes",
-                data: ''
-            });   
-        }else{
-            
-            res.render('notes',
-            {
-                page_title: "Notes",
-                data: rows
-            });
-        }                          
-         });
-    });
+router.get("/", function (req, res, next) {
+  connection.query("SELECT * FROM notes ORDER BY id", function (err, rows) {
+    if (err) {
+      //req.flash('error', err);
+      res.render("notes", {
+        page_title: "Notes",
+        data: "",
+      });
+    } else {
+      res.render("notes", {
+        page_title: "Notes",
+        data: rows,
+      });
+    }
+  });
+});
 
 /* GET notes edit page. */
-router.get('/notes/edit/:id', function(req, res, next) {
-      
-    connection.query('SELECT * FROM notes WHERE Id='+ req.params.id, function(err,row)     {
-    
-        if (err) {
-            //req.flash('error', err);
-            res.render("notes", {
-              page_title: "Notes ",
-              project: "",
-            });
-          } else {
-            res.render("notes", {
-              page_title: "Notes",
-              note: row,
-            });
-          }
-        }
-      );
-    });
+router.get("/notes/edit/:id", function (req, res, next) {
+  connection.query(
+    "SELECT * FROM notes WHERE Id=" + req.params.id,
+    function (err, row) {
+      if (err) {
+        //req.flash('error', err);
+        res.render("notes", {
+          page_title: "Notes ",
+          project: "",
+        });
+      } else {
+        res.render("notes", {
+          page_title: "Notes",
+          note: row,
+        });
+      }
+    }
+  );
+});
 
-    /* GET create notes page. */
+/* GET create notes page. */
 router.get("/create-project", (req, res, next) => {
   res.render("projects/create");
 });
 
 module.exports = router;
+
+// add
+router.post("/add", (req, res) => {
+  const data = {
+    note: req.body.note,
+    active_date: req.body.active_date,
+    project_id: req.body.project_id,
+    id: req.body.id,
+  };
+
+  connection.query(`INSERT INTO projects SET ?`, data, (err, results) => {
+    if (err) throw err;
+
+    res.redirect("/projects");
+  });
+});
--- routes/projects.js
+++ routes/projects.js
@@ -2,11 +2,6 @@ var express = require("express");
 var router = express.Router();
 var connection = require("../lib/db");
 
-/* GET create project page. */
-// router.get("/create-project", (req, res, next) => {
-//   res.render("projects/create");
-// });
-
 /* GET projects page. */
 router.get("/", function (req, res, next) {
   connection.query("SELECT * FROM projects ORDER BY id", function (err, rows) {
@@ -26,7 +21,7 @@ router.get("/", function (req, res, next) {
 });
 
 /* GET projects edit page. */
-router.get("/projects/edit/:id", function (req, res, next) {
+router.get("/edit/:id", function (req, res, next) {
   connection.query(
     "SELECT * FROM projects WHERE id=" + req.params.id,
     function (err, row) {
@@ -47,7 +42,7 @@ router.get("/projects/edit/:id", function (req, res, next) {
 });
 
 /* POST projects edit page. */
-router.post("/projects/update/", function (req, res, next) {
+router.post("/projects/edit/", function (req, res, next) {
   let sqlQuery =
     "UPDATE projects SET project_title ='" +
     req.body.project_title +
@@ -67,6 +62,48 @@ router.post("/projects/update/", function (req, res, next) {
   });
 });
 
+// update
+// router.get("/update/:id", (req, res) => {
+//   const data = {
+//     project_title: req.body.project_title,
+//     project_description: req.body.project_description,
+//     project_start_dt: req.body.project_start_dt,
+//     project_due_dt: req.body.project_due_dt,
+//   };
+
+//   connection.query(
+//     `UPDATE projects SET ? WHERE id = ${req.params.id}`,
+//     data,
+//     (err, results) => {
+//       if (err) throw err;
+
+//       res.redirect("/projects");
+//     }
+//   );
+// });
+
+  //  SELECT * FROM projects WHERE id = ' + req.params.id, (err, row) => {
+//       if(err){
+//           console.log(err)
+//       } else {
+//           console.log(row[0])
+
+//           // dynamically change the fields of the edited project to sync with its project
+//           // new Date(<date>).toISOString().split('T')[0]
+
+//           res.render('add', {
+//               id: data[i].id,
+//               project_title: data[i].project_title,
+//               project_description: data[i].project_description,
+//               project_start_dt: new Date(data[i].project_start_dt).toDateString('fr-CA').split('T')[i],
+//               project_due_dt: new Date(data[i].project_due_dt).toDateString('fr-CA').split('T')[i]  
+              
+//           });
+//       }
+//   })
+// })
+
+
 
 
 // add
@@ -81,72 +118,26 @@ router.post("/add", (req, res) => {
   connection.query(`INSERT INTO projects SET ?`, data, (err, results) => {
     if (err) throw err;
 
-    res.redirect("/projects")
-
+    res.redirect("/projects");
   });
-});
-
-// update
-router.put("/update/:id", (req, res) => {
-  const data = {
-    project_title: req.body.project_title,
-    project_description: req.body.project_description,
-    project_start_dt: req.body.project_start_dt,
-    project_due_dt: req.body.project_due_dt,
-  };
-
-  conn.query(
-    `UPDATE projects SET ? WHERE id = ${req.params.id}`,
-    data,
-    (err, results) => {
-      if (err) throw err;
-
-      res.redirect("/projects")
-    }
-  );
 });
 
 // delete
-router.delete("/delete/:id", (req, res) => {
+
+router.get("/delete/:id", (req, res, next) => {
   const id = Number(req.params.id);
   if (id == NaN) error(res, "use a number for the projects id", 422);
 
-  conn.query(`DELETE FROM projects WHERE id = ${id};`, (err, results) => {
-    if (err) throw err;
-    res.redirect("/projects")
-
+  connection.query(`DELETE FROM projects WHERE id = ${id};`, (err, row) => {
+    if (err) {
+      req.flash("error", err);
+    } else {
+      req.flash("success", "Deleted successfully!!");
+      res.redirect("/projects");
+      next();
+    }
   });
 });
-
-
-
-
-
-
-
-
-
-
-// function success(response, data = [], message = "success", status = 200) {
-//   response.send({ status, message, data });
-// }
-
-// function error(response, message = "error", status = 500, data = []) {
-//   response.send({ status, message, data });
-// } 
-
-
-
-
-
-// // index view
-// router.get('/', (req, res) => {
-// 	conn.query('SELECT id,title,first_nm,last_nm FROM amberapp1.trainers;', (err, results) => {
-// 		if (err) throw err.toString()
-
-// 		res.render('trainer/index', { trainers: results })
-// 	})
-// })
 
 // edit view
 // router.get("/edit/:id", (req, res) => {
